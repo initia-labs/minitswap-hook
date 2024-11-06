d1ce348b046669b73b869cfc16ca526ce046b39b
suha jin <89185836+djm07073@users.noreply.github.com>
2024-11-06 12:05:59 +0900

    feat: minitswap hook for evm

---
+++ evm/minitswap-hook.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import "./i_cosmos/ICosmos.sol";
+
+contract MinitSwapHook {
+    function minitswap_hook(
+        string memory denom,
+        uint amount,
+        string memory receiver
+    ) public {
+        string memory message = string(
+            abi.encodePacked(
+                '{"@type": "/opinit.opchild.v1.MsgInitiateTokenWithdrawal",',
+                '"sender": "',
+                COSMOS_CONTRACT.to_cosmos_address(msg.sender),
+                '","to":"',
+                receiver,
+                '","amount": Coin { denom:',
+                denom,
+                ",amount:",
+                amount,
+                "}}"
+            )
+        );
+        COSMOS_CONTRACT.execute_cosmos(message);
+    }
+}
